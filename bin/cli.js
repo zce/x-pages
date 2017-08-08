929b2079f1cf170ca1bf6f7eefaa254a4f8aa70e
zce <w@zce.me>
2017-08-08 18:49:35 +0800

    Add version tip & command limit

--- bin/cli.js
+++ bin/cli.js
@@ -2,13 +2,41 @@
 
 const { join } = require('path')
 const { log, colors } = require('gulp-util')
+const { name, version } = require('../package')
 
 const { env, argv, cwd } = process
 
-if (!env.NODE_ENV) {
+// version
+if (['-v', '--version'].includes(argv[2])) {
+  return console.log(colors.red('v' + version))
+}
+
+// task scope
+if (!['init', 'serve', 'build', 'deploy', 'serve:dist'].includes(argv[2])) {
+  return console.log(`
+command '${argv[2]}' not found
+
+Usage:
+  ${name} <command> [options]
+
+commands:
+  init
+  serve
+  build
+  deploy
+  serve:dist
+
+options:
+  --production
+`)
+}
+
+// mode
+if (env.NODE_ENV === undefined) {
   env.NODE_ENV = argv.includes('--production') ? 'production' : 'development'
 }
 
+// bootstrap gulp
 log('Running', colors.red(argv[2]), 'in', colors.blue(env.NODE_ENV), 'mode')
 
 argv.push('--cwd')
